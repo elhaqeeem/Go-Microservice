657dc103bcbffe45a8d4a242706233bbdce5cfff
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-27 05:30:35 +0000

    (feat): add uuid

--- account/logic.go
+++ account/logic.go
@@ -1,10 +1,36 @@
 package account
 
 import (
+	"context"
+
 	"github.com/go-kit/kit/log"
+	"github.com/gofrs/uuid"
 )
 
 type service struct {
 	repository Repository
 	logger     log.Logger
+}
+
+func NewService(rep Repository, logger log.Logger) Service {
+	return &service{
+		repository: rep,
+		logger:     logger,
+	}
+}
+
+func (s service) CreateUser(ctx context.Context, email string, password string) (string, error) {
+	logger := log.With(s.logger, "method", "CreateUser")
+
+	uuid, _ := uuid.NewV4()
+	id := uuid.String()
+	user := User{
+		ID:       id,
+		Email:    email,
+		Password: password,
+	}
+}
+
+func (s service) GetUser(ctx context.Context, id string) (string, error) {
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/abhinavrobinson/Go-Microservice
 
 go 1.16
 
-require github.com/go-kit/kit v0.10.0
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/gofrs/uuid v4.0.0+incompatible
+)
