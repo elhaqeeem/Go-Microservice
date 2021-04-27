eda21df030210afbee74180828718427b90742e1
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-27 05:56:43 +0000

    (feat): add CreateUser Endpoint

--- account/endpoint.go
+++ account/endpoint.go
@@ -1,8 +1,31 @@
 package account
 
-import "github.com/go-kit/kit/endpoint"
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+)
 
 type Endpoints struct {
 	CreateUser endpoint.Endpoint
 	GetUser    endpoint.Endpoint
+}
+
+func MakeEndpoints(s Service) Endpoints {
+	return Endpoints{
+		CreateUser: makeCreateUserEndpoint(s),
+		GetUser:    makeGetUserEndpoint(s),
+	}
+}
+
+func makeCreateUserEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(CreateUserRequest)
+		ok, err := s.CreateUser(ctx, req.Email, req.Password)
+		return CreateUserResponce{Ok: ok}, err
+	}
+}
+
+func makeGetUserEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {}
 }
